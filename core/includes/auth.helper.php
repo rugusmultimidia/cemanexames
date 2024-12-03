dd577b2d059d4675e834fd99c5867fcd286ea41e
rugusmultimidia <rugus.digital@gmail.com>
2024-12-03 17:21:57 -0300

    ajustes no sistema

--- core/includes/auth.helper.php
+++ core/includes/auth.helper.php
@@ -222,7 +222,7 @@ class Auth {
                 LIMIT 1
                 ";
 
-                // die(json_encode($q));
+                die(json_encode($q));
 
                 $sql = $db->executeSql($q);
 
