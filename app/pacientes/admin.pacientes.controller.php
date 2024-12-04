8183c9d82f7bde708d8c8a817e0f3bba1547d1df
rugusmultimidia <rugus.digital@gmail.com>
2024-12-04 18:03:13 -0300

    ajuste no declaracao

--- app/pacientes/admin.pacientes.controller.php
+++ app/pacientes/admin.pacientes.controller.php
@@ -89,6 +89,7 @@ class pacientes extends Controller {
 			$dados['exames'] = $this->exames_model->getIdPacienteResultSite($id_pacientes);
 
 			foreach ($dados['exames'] as &$exame) {
+				die(var_dump($exame));
 				if (!empty($exame['pdf']) && !empty($exame['exame']) && $exame['exame'] != '') {
 					
 					$exame['pdf'] = unserialize($exame['pdf']);
